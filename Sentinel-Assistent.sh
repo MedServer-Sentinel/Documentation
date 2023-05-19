f26c296703c5bcc7bb071605d51141f8d9983f95
CaiqueGomesdaSilva <@caique.gsilva@sptech.school>
2023-05-19 14:35:38 -0300

    script

--- Sentinel-Assistent.sh
+++ Sentinel-Assistent.sh
@@ -8,162 +8,149 @@ pasta_origem="$HOME/Desktop/Backend-MedControll"
 diretorio_destino="$HOME/Documents/"
 
 sql='
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE = @@SQL_MODE;
-SET @@SQL_MODE = CONCAT(@@SQL_MODE, ',NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION');
 CREATE SCHEMA IF NOT EXISTS bd-medserver-sentinel DEFAULT CHARACTER SET utf8 ;
 USE bd-medserver-sentinel ;
 
 CREATE TABLE IF NOT EXISTS Maquina (
-  id_maquina INT NOT NULL auto_increment,
-  nome VARCHAR(45),
-  tipo VARCHAR(45),
-  cod_MAC VARCHAR(45) NOT NULL,
-  andar VARCHAR(45),
-  setor VARCHAR(45),
-  PRIMARY KEY (id_maquina))
+id_maquina INT NOT NULL auto_increment,
+nome VARCHAR(45),
+tipo VARCHAR(45),
+cod_MAC VARCHAR(45) NOT NULL,
+andar VARCHAR(45),
+setor VARCHAR(45),
+PRIMARY KEY (id_maquina))
 ENGINE = InnoDB;
 
 INSERT INTO Maquina VALUES(null,"Maquina de testes","Notebook","BFEBFBFF000806D1",1,"T.I");
 
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS MemoriaRam (
-  id_ram INT NOT NULL auto_increment,
-  capacidade_total VARCHAR(45) NOT NULL,
-  fk_maquina INT UNIQUE NOT NULL,
-  PRIMARY KEY (id_ram),
-  CONSTRAINT fk_ram_Maquina1
-    FOREIGN KEY (fk_maquina)
-    REFERENCES Maquina (id_maquina)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+id_ram INT NOT NULL auto_increment,
+capacidade_total VARCHAR(45) NOT NULL,
+fk_maquina INT UNIQUE NOT NULL,
+PRIMARY KEY (id_ram),
+CONSTRAINT fk_ram_Maquina1
+FOREIGN KEY (fk_maquina)
+REFERENCES Maquina (id_maquina)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS DadosRam (
-  id_dados_ram INT NOT NULL auto_increment,
-  em_uso VARCHAR(45) NOT NULL,
-  disponivel VARCHAR(45)NOT NULL,
-  data_hora DATETIME NOT NULL,
-  fk_ram INT NOT NULL,
-  PRIMARY KEY (id_dados_ram),
-  CONSTRAINT fk_dados_ram_ram1
-    FOREIGN KEY (fk_ram)
-    REFERENCES MemoriaRam (id_ram)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+id_dados_ram INT NOT NULL auto_increment,
+em_uso VARCHAR(45) NOT NULL,
+disponivel VARCHAR(45)NOT NULL,
+data_hora DATETIME NOT NULL,
+fk_ram INT NOT NULL,
+PRIMARY KEY (id_dados_ram),
+CONSTRAINT fk_dados_ram_ram1
+FOREIGN KEY (fk_ram)
+REFERENCES MemoriaRam (id_ram)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
 CREATE TABLE IF NOT EXISTS Processador (
-  id_processador INT NOT NULL auto_increment,
-  nome_processador VARCHAR(200) NOT NULL,
-  frequencia VARCHAR(45) NOT NULL,
-  num_nucleo INT NOT NULL,
-  fk_maquina INT UNIQUE NOT NULL,
-  PRIMARY KEY (id_processador),
-  CONSTRAINT fk_cpu_Maquina1
-    FOREIGN KEY (fk_maquina)
-    REFERENCES Maquina (id_maquina)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+id_processador INT NOT NULL auto_increment,
+nome_processador VARCHAR(200) NOT NULL,
+frequencia VARCHAR(45) NOT NULL,
+num_nucleo INT NOT NULL,
+fk_maquina INT UNIQUE NOT NULL,
+PRIMARY KEY (id_processador),
+CONSTRAINT fk_cpu_Maquina1
+FOREIGN KEY (fk_maquina)
+REFERENCES Maquina (id_maquina)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Processo (
-  id_processo INT NOT NULL auto_increment,
-  pid INT NOT NULL,
-  nome VARCHAR(45) NOT NULL,
-  uso_cpu DOUBLE NOT NULL,
-  uso_ram DOUBLE NOT NULL,
-  fk_processador INT NOT NULL,
-  PRIMARY KEY (id_processo),
-  CONSTRAINT fk_pross_cpu1
-    FOREIGN KEY (fk_processador)
-    REFERENCES Processador (id_processador)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+id_processo INT NOT NULL auto_increment,
+pid INT NOT NULL,
+nome VARCHAR(45) NOT NULL,
+uso_cpu DOUBLE NOT NULL,
+uso_ram DOUBLE NOT NULL,
+fk_processador INT NOT NULL,
+PRIMARY KEY (id_processo),
+CONSTRAINT fk_pross_cpu1
+FOREIGN KEY (fk_processador)
+REFERENCES Processador (id_processador)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Janela (
-  id_janela INT NOT NULL auto_increment,
-  pid INT NOT NULL,
-  titulo VARCHAR(200) NOT NULL,
-  comando VARCHAR(200) NOT NULL,
-  fk_processador INT NOT NULL,
-  PRIMARY KEY (id_janela),
-  CONSTRAINT fk_janela_cpu1
-    FOREIGN KEY (fk_processador)
-    REFERENCES Processador (id_processador)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+id_janela INT NOT NULL auto_increment,
+pid INT NOT NULL,
+titulo VARCHAR(200) NOT NULL,
+comando VARCHAR(200) NOT NULL,
+fk_processador INT NOT NULL,
+PRIMARY KEY (id_janela),
+CONSTRAINT fk_janela_cpu1
+FOREIGN KEY (fk_processador)
+REFERENCES Processador (id_processador)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Disco (
-  id_disco INT NOT NULL auto_increment,
-	nome VARCHAR(45) NOT NULL,
-    tipo VARCHAR(45) NOT NULL,
-    total VARCHAR(45) NOT NULL,
-    disponivel VARCHAR(45) NOT NULL,
-    uuid VARCHAR(45) NOT NULL,
-  fk_maquina INT NOT NULL,
-  PRIMARY KEY (id_disco),
-  CONSTRAINT fk_disco_Maquina1
-    FOREIGN KEY (fk_maquina)
-    REFERENCES Maquina (id_maquina)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+id_disco INT NOT NULL auto_increment,
+nome VARCHAR(45) NOT NULL,
+tipo VARCHAR(45) NOT NULL,
+total VARCHAR(45) NOT NULL,
+disponivel VARCHAR(45) NOT NULL,
+uuid VARCHAR(45) NOT NULL,
+fk_maquina INT NOT NULL,
+PRIMARY KEY (id_disco),
+CONSTRAINT fk_disco_Maquina1
+FOREIGN KEY (fk_maquina)
+REFERENCES Maquina (id_maquina)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
 CREATE TABLE IF NOT EXISTS DadosDisco (
-  id_dados_disco INT NOT NULL auto_increment,
-  velocida_escrita  VARCHAR(45) NOT NULL,
-  velocidade_leitura  VARCHAR(45) NOT NULL,
-  tempo_escrita  VARCHAR(45) NOT NULL,
-  data_hora DATETIME NOT NULL,
-  fk_disco INT NOT NULL,
-  PRIMARY KEY (id_dados_disco),
-  CONSTRAINT fk_Disco_disco1
-    FOREIGN KEY (fk_disco)
-    REFERENCES Disco (id_disco)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+id_dados_disco INT NOT NULL auto_increment,
+velocida_escrita VARCHAR(45) NOT NULL,
+velocidade_leitura VARCHAR(45) NOT NULL,
+tempo_escrita VARCHAR(45) NOT NULL,
+data_hora DATETIME NOT NULL,
+fk_disco INT NOT NULL,
+PRIMARY KEY (id_dados_disco),
+CONSTRAINT fk_Disco_disco1
+FOREIGN KEY (fk_disco)
+REFERENCES Disco (id_disco)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Parametro (
-  id_parametro int auto_increment not null,
-  significativo INT NOT NULL,
-  moderado INT NOT NULL,
-  critico INT NOT NULL,
-  fk_maquina INT NOT NULL,
-  PRIMARY KEY (id_parametro),
-  CONSTRAINT fk_parametros_Maquina1
-    FOREIGN KEY (fk_maquina)
-    REFERENCES Maquina (id_maquina)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+id_parametro int auto_increment not null,
+significativo INT NOT NULL,
+moderado INT NOT NULL,
+critico INT NOT NULL,
+fk_maquina INT NOT NULL,
+PRIMARY KEY (id_parametro),
+CONSTRAINT fk_parametros_Maquina1
+FOREIGN KEY (fk_maquina)
+REFERENCES Maquina (id_maquina)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 CREATE TABLE IF NOT EXISTS SistemaOperacional (
-  id_sistema INT NOT NULL auto_increment,
-  fabricante VARCHAR(45) NOT NULL,
-  tipo_sistema VARCHAR(45) NOT NULL,
-  arquitetura VARCHAR(45) NOT NULL,
-  tempo_atividade VARCHAR(45) NOT NULL,
-  fk_maquina INT UNIQUE NOT NULL,
-  PRIMARY KEY (id_sistema),
-  CONSTRAINT fk_SISTEMA_OPERACIONAL_Maquina1
-    FOREIGN KEY (fk_maquina)
-    REFERENCES Maquina (id_maquina)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;'
+id_sistema INT NOT NULL auto_increment,
+fabricante VARCHAR(45) NOT NULL,
+tipo_sistema VARCHAR(45) NOT NULL,
+arquitetura VARCHAR(45) NOT NULL,
+tempo_atividade VARCHAR(45) NOT NULL,
+fk_maquina INT UNIQUE NOT NULL,
+PRIMARY KEY (id_sistema),
+CONSTRAINT fk_SISTEMA_OPERACIONAL_Maquina1
+FOREIGN KEY (fk_maquina)
+REFERENCES Maquina (id_maquina)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
+ENGINE = InnoDB;'
 	
 echo  "$(tput setaf 10)[Sentinel Bot]:$(tput setaf 7) : Olá usuário, me chamo Sentinel e serei seu assistente para instalação do Java!;"
 sleep 2
